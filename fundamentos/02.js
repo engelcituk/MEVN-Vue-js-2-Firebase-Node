d76d0962083ea7e04850265a6489ce7ec733373f
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-20 15:46:29 -0500

    Clases (v-bind:class), manejo de clases, css con vuejs

---
+++ fundamentos/02.js
@@ -0,0 +1,13 @@
+// instancio vue
+const app = new Vue({
+    el: '#app', // obtengo el identificador
+    data: {
+        fondo: 'bg-warning',
+        color: true
+    }, methods: {
+        
+    },
+    computed: {
+        
+    }
+})
