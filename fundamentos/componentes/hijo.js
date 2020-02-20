948d5779f99bd0db99f7128253b4ae6362bc8b26
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-20 17:43:29 -0500

    Comunicación entre componentes (parte 1), enviar datos del hijo al padre en componentes

--- fundamentos/componentes/hijo.js
+++ fundamentos/componentes/hijo.js
@@ -2,8 +2,19 @@ Vue.component('hijo', {
     template:
         `
         <div class="py-5 bg-warning text-white">
-            <h4>Componente hijo {{numero}}</h4>                    
+            <h4>Componente hijo {{numero}}</h4>  
+            <h4>Nombre: {{nombre}}</h4>                    
+            <button class="btn btn-danger" @click="numero++">+</button>
+
         </div>                
         `,
-    props: ['numero']
+    data() {
+        return {
+            nombre: "Soy el nombre del hijo"
+        }
+    },
+    mounted(){
+        this.$emit('nombreHijo', this.nombre); //envio algo para usar en el componente padre
+    },
+    props: ['numero'] // para recibir algo desde el padre se usan los props
 });
--- fundamentos/componentes/padre.js
+++ fundamentos/componentes/padre.js
@@ -3,13 +3,15 @@ Vue.component('padre', {
         `
         <div class="p-5 bg-dark text-white">
             <h3>Componente padre {{numeroPadre}}</h3>
-            <hijo :numero="numeroPadre"></hijo> 
             <button class="btn btn-danger" @click="numeroPadre++">+</button>
+            {{nombrePadreDesdeHijo}}
+            <hijo :numero="numeroPadre" @nombreHijo="nombrePadreDesdeHijo = $event"></hijo> 
         </div>                
         `,
     data() {
         return {
-            numeroPadre: 3
+            numeroPadre: 3,
+            nombrePadreDesdeHijo: ''
         }
     }
 });
