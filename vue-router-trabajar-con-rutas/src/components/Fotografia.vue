8f4623f1ae7e9fe5f25cd6451388dce39a7de8ce
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-21 14:48:59 -0500

    router-link y rutas con parámetros

---
+++ vue-router-trabajar-con-rutas/src/components/Fotografia.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fotos">
+    <h1>foto desde componente foto: {{$route.params.id}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Fotografia',
+}
+</script>
---
+++ vue-router-trabajar-con-rutas/src/views/Fotos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fotos">
+      <h1>Galeria de fotos</h1>
+    <Foto/>
+  </div>
+</template>
+
+
+<script>
+import Foto from '@/components/Fotografia.vue'
+export default {
+    components:{
+        Foto
+    }
+}
+</script>
