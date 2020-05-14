87ccdb40f2023621b89931be9fd523929a97db90
cituk <programaciondyn3.mx@sandos.com>
2020-05-14 02:04:09 -0500

    Agregar Usuario: Vuex

--- auth-firebase/src/views/Registro.vue
+++ auth-firebase/src/views/Registro.vue
@@ -1,16 +1,20 @@
+import { mapActions } from 'vuex';
 <template>
     <div>
         <h1>Registro de usuarios</h1>
-        <form >
+        <form @submit.prevent="crearUsuario({email:email, password:password1})">
             <input type="email" placeholder="email" v-model="email">
             <input type="password" placeholder="contraseña" v-model="password1">
             <input type="password" placeholder="confirmar contraseña" v-model="password2">
             <button type="submit">Crear usuario</button>
         </form>
+        <p>{{error}}</p>
     </div>
 </template>
 
 <script>
+import {mapActions, mapState} from 'vuex'
+
 export default {
     name: 'Registro',
     data(){
@@ -19,6 +23,12 @@ export default {
             password1:'',
             password2:''
         }
+    },
+    methods: {
+        ...mapActions(['crearUsuario'])
+    },
+    computed:{
+        ...mapState(['error'])
     }
 }
 </script>
