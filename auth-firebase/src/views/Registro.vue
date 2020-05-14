2e865256a3b68043f58a717ed0ba9785316066f6
cituk <programaciondyn3.mx@sandos.com>
2020-05-14 01:39:50 -0500

    Agregar Usuario: Ruta y vista

---
+++ auth-firebase/src/views/Registro.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Registro de usuarios</h1>
+        <form >
+            <input type="email" placeholder="email" v-model="email">
+            <input type="password" placeholder="contraseña" v-model="password1">
+            <input type="password" placeholder="confirmar contraseña" v-model="password2">
+            <button type="submit">Crear usuario</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Registro',
+    data(){
+        return {
+            email: '',
+            password1:'',
+            password2:''
+        }
+    }
+}
+</script>
