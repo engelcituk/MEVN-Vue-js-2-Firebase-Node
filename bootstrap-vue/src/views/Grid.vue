49434a3fe5de45da1e01038981854317fb4983ab
cituk <programaciondyn3.mx@sandos.com>
2020-05-16 15:19:25 -0500

    Columnas responsives (Row/Col)

---
+++ bootstrap-vue/src/views/Grid.vue
@@ -0,0 +1,18 @@
+import md5 from 'md5';
+<template>
+    <div class="mt-5">
+        <b-row>
+            <b-col cols="12" md="6">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim atque explicabo dolorum esse, amet quam corporis asperiores quo dolorem! Libero eaque omnis eum! Accusamus sequi perspiciatis alias cum reiciendis?</p>
+            </b-col>
+            <b-col cols="12" md="6">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim atque explicabo dolorum esse, amet quam corporis asperiores quo dolorem! Libero eaque omnis eum! Accusamus sequi perspiciatis alias cum reiciendis?</p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+export default {
+    name:'Grid',
+}
+</script>
