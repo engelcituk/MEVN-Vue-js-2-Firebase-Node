ea0e883cd0d8485c724c90817807d7dd4b6a1552
cituk <programaciondyn3.mx@sandos.com>
2020-05-17 15:13:32 -0500

    Validando Ingreso de usuarios

--- auth-firebase/src/views/Ingreso.vue
+++ auth-firebase/src/views/Ingreso.vue
@@ -1,16 +1,27 @@
 <template>
     <div>
         <h1>Ingreso de usuario</h1>
-        <form @submit.prevent="ingreseUsuario({email, password})">
-            <input type="email" placeholder="email" v-model="email">
-            <input type="password" placeholder="contraseña" v-model="password">
-            <button type="submit" class="btn btn-success">Ingresar</button>
+        <form @submit.prevent="ingreseUsuario({email:$v.email.$model, password:$v.password.$model})">
+            <input class="form-control my-2" type="email" placeholder="email" v-model="$v.email.$model">
+            <small class="text-danger d-block" v-if="!$v.email.required">Campo requerido</small>
+            <small class="text-danger d-block" v-if="!$v.email.email">Email no válido</small>
+
+
+            <input class="form-control my-2" type="password" placeholder="contraseña" v-model="$v.password.$model">
+            <small class="text-danger d-block" v-if="!$v.password.required">Campo requerido</small>
+            <small class="text-danger d-block" v-if="!$v.password.minLength">Minimo 6 caracteres</small>
+
+
+            <button type="submit" class="btn btn-info" :disabled="!$v.invalid"> Ingresar </button>
         </form>
         <p>{{error}}</p>
+        <p>{{$v.email}}</p>
     </div>
 </template>
 <script>
 import {mapActions, mapState} from 'vuex'
+import { required, email, minLength} from 'vuelidate/lib/validators';
+
 export default {
     name: 'Ingreso',
     data(){
@@ -24,6 +35,10 @@ export default {
     },
     computed: {
         ...mapState(['error'])
+    },
+    validations:{
+        email:{email, required},
+        password: {required, minLength: minLength(6)}
     }
 }
 </script>
